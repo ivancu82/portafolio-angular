5962a8b1da07e9569319dde25d2dc77d85de4da2
ivancu82 <ivan.cuenca82@gmail.com>
2020-02-23 01:29:46 +0100

    Automatizacion con Firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,32 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
      constructor( private http: HttpClient) {
-
     // console.log('Servicio de infoPagina listo');
+      this.cargarInfo();
+      this.cargarEquipo();
+  }
 
+  private cargarInfo() {
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
-        .subscribe( (resp: InfoPagina) => {
+      .subscribe( (resp: InfoPagina) => {
 
-          this.cargada = true;
-          this.info = resp;
-          console.log(resp);
+        this.cargada = true;
+        this.info = resp;
+        // console.log(resp);
       });
-  }
+    }
+
+    private cargarEquipo() {
+      // Leer el archivo JSON
+      this.http.get('https://angularwebapp-c55b4.firebaseio.com/equipo.json')
+        .subscribe( (resp: any[]) => {
+          this.equipo = resp;
+          console.log(resp);
+        });
+    }
 }
 
